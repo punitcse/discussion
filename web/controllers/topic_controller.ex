639a77c187bd577d7b4a7614b46a832ff68c7183
Punit Jain <jainpunit786@gmail.com>
2017-04-06 17:36:23 +0530

    add edit and delete and some refactoring

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -48,4 +48,22 @@ defmodule Discuss.TopicController do
         render conn, "edit.html", changeset: changeset, topic: old_topic
     end
   end
+
+  def delete(conn, %{"id" => topic_id }) do
+    Repo.get!(Topic, topic_id) |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "topic has benn delete successfully")
+    |> redirect(to: topic_path(conn, :index))
+    # case Repo.delete(topic) do
+    #   {:ok, _topic} ->
+    #     conn
+    #     |> put_flash(:info, to: topic_path(conn, index))
+    #     |> redirect(to: topic_path(conn, :index))
+    #   {:error, _changeset} ->
+    #     conn
+    #     |> put_flash(:error, to: topic_path)
+    #     |> redirect(to: topic_path(conn, :index))
+    # end
+  end
 end
